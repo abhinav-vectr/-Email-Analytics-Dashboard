21a92b7fac5c2fc9940e5ad548a04d2da2979d74
AbhinavPentani <abhinavpentani7@gmail.com>
2026-04-13 23:40:13 +0530

    Fix python imports for Render

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ from datetime import datetime
 from collections import Counter
 import re
 
-from database import get_db, engine
-from models import Base, Email
+from backend.database import get_db, engine
+from backend.models import Base, Email
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
